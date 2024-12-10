a5fd96d27a7741355b4fac133ae78460137b53b0
fasta <fastakovskiy.mihail04@gmail.com>
2024-12-10 02:58:43 +0200

    Add project files.

---
+++ NetCsharpWinFormsCrestNuls/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace NetCsharpWinFormsCrestNuls
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 125);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(164, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 125);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button3_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(297, 26);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 125);
+            button3.TabIndex = 0;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(31, 157);
+            button4.Name = "button4";
+            button4.Size = new Size(127, 125);
+            button4.TabIndex = 0;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button3_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(164, 157);
+            button5.Name = "button5";
+            button5.Size = new Size(127, 125);
+            button5.TabIndex = 0;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button3_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(297, 157);
+            button6.Name = "button6";
+            button6.Size = new Size(127, 125);
+            button6.TabIndex = 0;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button3_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(31, 288);
+            button7.Name = "button7";
+            button7.Size = new Size(127, 125);
+            button7.TabIndex = 0;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button3_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(164, 288);
+            button8.Name = "button8";
+            button8.Size = new Size(127, 125);
+            button8.TabIndex = 0;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button3_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(297, 288);
+            button9.Name = "button9";
+            button9.Size = new Size(127, 125);
+            button9.TabIndex = 0;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button3_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(501, 26);
+            button10.Name = "button10";
+            button10.Size = new Size(204, 57);
+            button10.TabIndex = 1;
+            button10.Text = "НАЧАТЬ ИГРУ";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(501, 109);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(172, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Первым ходит компьютер";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(checkBox1);
+            Controls.Add(button10);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private CheckBox checkBox1;
+    }
+}
---
+++ NetCsharpWinFormsCrestNuls/Form1.cs
@@ -0,0 +1,193 @@
+namespace NetCsharpWinFormsCrestNuls
+{
+
+
+    public partial class Form1 : Form
+    {
+        bool isStarted { get; set; }
+        bool isFinish { get; set; }
+       List<MyButton> buttons;
+        public Form1()
+        {
+            InitializeComponent();
+           
+            buttons = new List<MyButton>();
+            buttons.Add(new MyButton(button1));
+            buttons.Add(new MyButton(button2));
+            buttons.Add(new MyButton(button3));
+            buttons.Add(new MyButton(button4));
+            buttons.Add(new MyButton(button5));
+            buttons.Add(new MyButton(button6));
+            buttons.Add(new MyButton(button7));
+            buttons.Add(new MyButton(button8));
+            buttons.Add(new MyButton(button9));
+        }
+        public void Check(string b)
+        {
+            if (b == "X")
+            {
+                MessageBox.Show("Выиграли крестики", "Крестики-Нолики", MessageBoxButtons.OK);
+                this.Close();
+                return;
+            }
+            else if (b == "O")
+                {
+                    MessageBox.Show("Выиграли нолики", "Крестики-Нолики", MessageBoxButtons.OK);
+                this.Close();
+                return;
+                }
+           
+        }
+        //start button
+        private void button10_Click(object sender, EventArgs e)
+        {
+             isStarted = true;
+            
+            if (checkBox1.Checked)
+            {
+                Bitmap img = new Bitmap("X.jpg");
+                
+                foreach (var button in buttons) { 
+                if (!button.isOpen)
+                    {
+                        button.butt.Image = img;
+                        button.who = "X";
+                        button.isOpen = true;
+                        break;
+                    }
+                }
+            }
+        }
+        //icon_button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (isStarted==false)
+            {
+                MessageBox.Show("Игра еще не начата","Крестики-Нолики", MessageBoxButtons.OK);
+            }
+            if(!isFinish)
+            {
+               
+              
+                Bitmap img = new Bitmap("O.jpg");
+                Button b = (sender as Button);
+
+                if (buttons.Find(a => a.butt == b).isOpen)
+                {
+                    MessageBox.Show("Вы не можете занять это поле", "Крестики-Нолики", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    buttons.Find(a => a.butt == b).butt.Image = img;
+                    buttons.Find(a => a.butt == b).who = "O";
+                    buttons.Find(a => a.butt == b).isOpen = true;
+                    CheckComb();
+                    if (!isFinish)
+                    {
+                        Bitmap img2 = new Bitmap("X.jpg");
+                        foreach (var button in buttons)
+                        {
+                            if (!button.isOpen)
+                            {
+                                button.butt.Image = img2;
+                                button.who = "X";
+                                button.isOpen = true;
+                                break;
+                            }
+                        }
+                        CheckComb();
+                    }
+
+                }
+
+               
+              
+                  
+                
+
+
+
+
+
+
+                
+            }
+            
+        }
+        public void CheckComb()
+        {
+            string mybutton1 = buttons.Find(a => a.butt == button1).who;
+            string mybutton2 = buttons.Find(a => a.butt == button2).who;
+            string mybutton3 = buttons.Find(a => a.butt == button3).who;
+            string mybutton4 = buttons.Find(a => a.butt == button4).who;
+            string mybutton5 = buttons.Find(a => a.butt == button5).who;
+            string mybutton6 = buttons.Find(a => a.butt == button6).who;
+            string mybutton7 = buttons.Find(a => a.butt == button7).who;
+            string mybutton8 = buttons.Find(a => a.butt == button8).who;
+            string mybutton9 = buttons.Find(a => a.butt == button9).who;
+            if ((mybutton1 != "" && mybutton1 == mybutton2 && mybutton1 == mybutton3) || (mybutton4 != "" && mybutton4 == mybutton5 && mybutton4 == mybutton6) ||
+                              (mybutton7 != "" && mybutton7 == mybutton8 && mybutton7 == mybutton9) || (mybutton1 != "" && mybutton1 == mybutton4 && mybutton1 == mybutton7) ||
+                              (mybutton2 != "" && mybutton2 == mybutton5 && mybutton2 == mybutton8) || (mybutton3 != "" && mybutton3 == mybutton6 && mybutton3 == mybutton9) ||
+                              (mybutton1 != "" && mybutton1 == mybutton5 && mybutton1 == mybutton9) || (mybutton3 != "" && mybutton3 == mybutton5 && mybutton3 == mybutton7))
+            {
+                if (mybutton1 == mybutton2 && mybutton1 == mybutton3)
+                {
+                    Check(mybutton1);
+                    return;
+                }
+                else if (mybutton4 == mybutton5 && mybutton4 == mybutton6)
+                {
+                    Check(mybutton4);
+                    return;
+                }
+                else if (mybutton7 == mybutton8 && mybutton7 == mybutton9)
+                {
+                    Check(mybutton7);
+                    return;
+                }
+                else if (mybutton1 == mybutton4 && mybutton1 == mybutton7)
+                {
+                    Check(mybutton1);
+                    return;
+                }
+                else if (mybutton2 == mybutton5 && mybutton2 == mybutton8)
+                {
+                    Check(mybutton2);
+                    return;
+                }
+                else if (mybutton3 == mybutton6 && mybutton3 == mybutton9)
+                {
+                    Check(mybutton3);
+                    return;
+                }
+                else if (mybutton1 == mybutton5 && mybutton1 == mybutton9)
+                {
+                    Check(mybutton1);
+                    return;
+                }
+                else if (mybutton3 == mybutton5 && mybutton3 == mybutton7)
+                {
+                    Check(mybutton3);
+                    return;
+                }
+                isFinish = true;
+
+            }
+        }
+    }
+   
+    public class MyButton
+    {
+        public Button butt { get; set; }
+        public bool isOpen { get; set; }
+        public string who {  get; set; }
+        public MyButton() { }
+        public MyButton(Button b)
+        {
+            butt = b;
+            isOpen = false;
+            who = "";
+        }
+    }
+}
